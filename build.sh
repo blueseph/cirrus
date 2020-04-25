25639860e973925975becce21fdb7f0bbf3f8f1d
Dan Blanco <blueseph@gmail.com>
2020-04-25 02:52:45 -0700

    add workflow to create release and build file

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+NAME=cirrus
+OUTPUT=dist
+
+declare -A PLATFORMS=([linux]=linux [darwin]=osx [windows]=windows)
+declare -A ARCHITECTURES=([386]=i386 [amd64]=amd64)
+
+for platform in ${!PLATFORMS[@]}; do
+    for architecture in ${!ARCHITECTURES[@]}; do
+            full_name="${NAME}-${VERSION}_${PLATFORMS[$platform]}-${ARCHITECTURES[$architecture]}"
+            bin_name="$NAME"
+
+            if [ "$platform" == "windows" ]; then
+                bin_name="${NAME}.exe"
+            fi
+
+            mkdir -p "$OUTPUT_DIR/$full_name"
+
+            GOOS=$platform GOARCH=$architecture go build -o "$OUTPUT_DIR/${full_name}/${bin_name}"
+
+            zip -9 -r "$OUTPUT_DIR/${full_name}.zip" "$OUTPUT_DIR/$full_name"
+
+            rm -r "$OUTPUT_DIR/$full_name"
+    done
+done
